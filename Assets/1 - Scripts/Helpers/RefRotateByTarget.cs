42d8bda1b8cdd13cd5674d3c130a7dd05e4df467
Alexandr <belkapbigiy@gmail.com>
2022-04-19 17:56:54 +0300

    fix wrong spawn project tile Change dynamic joystick to static other fix

---
+++ Assets/1 - Scripts/Helpers/RefRotateByTarget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Helpers
+{
+    public class RefRotateByTarget : MonoBehaviour
+    {
+        public Transform Reference;
+        public Transform Target;
+
+        private void Update()
+        {
+            if(Reference && Target)Target.rotation = Reference.rotation;
+        }
+    }
+}
