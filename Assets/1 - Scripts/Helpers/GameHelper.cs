5df7be244ea0e62ba7a9e5d3b3ed16dd5907bb5a
Alexandr <belkapbigiy@gmail.com>
2022-04-22 14:42:12 +0300

    add particle on enemy damage

---
+++ Assets/1 - Scripts/Helpers/GameHelper.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class GameHelper : MonoBehaviour
+{
+    public void SpawnParticle(ParticleSystem system, Transform point, bool isParent)
+    {
+        var newSystem = Instantiate(system, point.position, point.rotation);
+        if(isParent) newSystem.transform.SetParent(point);
+    }
+}
